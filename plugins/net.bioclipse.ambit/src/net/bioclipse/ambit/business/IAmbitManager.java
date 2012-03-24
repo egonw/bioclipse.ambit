67f780e2c3534ff1e76765174f15e6a75e59dd88
Egon Willighagen <egon.willighagen@gmail.com>
2012-03-24 13:39:08 +0100

    Template for the ambit manager

---
+++ plugins/net.bioclipse.ambit/src/net/bioclipse/ambit/business/IAmbitManager.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2012  Egon Willighagen <egonw@users.sf.net>
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contact: http://www.bioclipse.net/
+ ******************************************************************************/
+package net.bioclipse.ambit.business;
+
+import net.bioclipse.core.PublishedClass;
+import net.bioclipse.managers.business.IBioclipseManager;
+
+@PublishedClass(
+    value="Manager exposing AMBIT functonality.",
+    doi="10.1186/1758-2946-3-18"
+)
+public interface IAmbitManager extends IBioclipseManager {
+
+}
